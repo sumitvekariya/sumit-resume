5478e80426f3b128d1dc08cd00a129d7e75bc1c0
Sumit <sumitvekariya7@gmail.com>
2020-12-05 20:15:35 +0530

    useHash true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,15 @@ export class AppComponent {
   constructor(private router: Router) {
     this.router.events.subscribe((e) => {
       if (e instanceof NavigationEnd) {
-        const node2 = document.createElement('script');
-        node2.src = 'assets/js/glitche-scripts.js';
-        node2.type = 'text/javascript';
-        node2.async = true;
-        document.getElementsByTagName('head')[0].appendChild(node2);
+        const removeScript = document.getElementById('script');
+        removeScript?.remove();
+
+        const node = document.createElement('script');
+        node.src = 'assets/js/glitche-scripts.js';
+        node.type = 'text/javascript';
+        node.async = true;
+        node.id = 'script';
+        document.getElementsByTagName('head')[0].appendChild(node);
       }
     });
   }
@@ -23,6 +27,6 @@ export class AppComponent {
   redirectTo(redirect: string): void {
     setTimeout(() => {
       this.router.navigate([redirect]);
-    }, 550);
+    }, 400);
   }
 }
